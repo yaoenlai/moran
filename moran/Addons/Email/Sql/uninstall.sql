c05411878de4e162921ef8cb9b3118960711efed
梁凯 <1399791106@qq.com>
2017-08-26 23:02:20 +0800

    update

--- moran/Addons/Email/Sql/uninstall.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS `oc_addon_email`;
--- moran/Application/Weixin/Sql/uninstall.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `oc_weixin_custom_menu`;
-DROP TABLE IF EXISTS `oc_weixin_custom_reply`;
-DROP TABLE IF EXISTS `oc_weixin_material`;
-DROP TABLE IF EXISTS `oc_weixin_user_bind`;
