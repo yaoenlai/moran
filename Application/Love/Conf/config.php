6a3c0266eda06a0f8dfb930e6c072eed10f2099a
kail <1399791106@qq.com>
2017-09-01 09:40:54 +0800

    去除重叠

--- Application/Love/Conf/config.php
+++ Application/Love/Conf/config.php
@@ -14,15 +14,6 @@ return array(
     'URL_ROUTE_RULES'   => array(
         ':uid\d'         => 'index/home',
     ),
-    //'DATA_CACHE_PREFIX' => 'Redis_',//缓存前缀
-    //'DATA_CACHE_TYPE'=>'Redis',//默认动态缓存为Redis
-    //'REDIS_RW_SEPARATE' => false, //Redis读写分离 true 开启
-    'REDIS_HOST'=>'127.0.0.1', //redis服务器ip，多台用逗号隔开；读写分离开启时，第一台负责写，其它[随机]负责读；
-    'REDIS_PORT'=>'6379',//端口号
-    'REDIS_TIMEOUT'=>'300',//超时时间
-    'REDIS_PERSISTENT'=>false,//是否长连接 false=短连接
-    'REDIS_AUTH'=>'junglecat@~127.0',//AUTH认证密码
-    'DATA_CACHE_TIME'       => 10800,      // 数据缓存有效期 0表示永久缓存
     // 套餐先粗糙配置，后期可以入库缓存
     'package' => [
         119 => [
