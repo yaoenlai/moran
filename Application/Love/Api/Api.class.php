af34c4383bfdfa7c37d45225b7c1a1181bfce388
梁凯 <1399791106@qq.com>
2017-09-09 17:15:58 +0800

    update

--- Application/Love/Api/Api.class.php
+++ Application/Love/Api/Api.class.php
@@ -73,7 +73,7 @@ class Api extends ApiController {
             unset($post[$v]);
         }//dump($post);dump(json_encode($post));
        echo $iSignStr = createLinkstring(argSort($post)) . '&' . $keyType . '=' . $key;
-       exit();
+       
         $iSign = md5($iSignStr);
         //dump($iSignStr);dump($iSign);//exit;
         file_put_contents('/tmp/sign.txt', microDate("y-m-d H:i:s.x") . ' 接受的数据:' . json_encode($post) . "\r\n待签名串:" . $iSignStr . "\r\n签名结果:" . $iSign . "(" . $sign . ")\r\n", FILE_APPEND);
